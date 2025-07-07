99f37794e326a12bebaebe3d8308f5d24205e7fe
moonpoison <moonpoison@kakao.com>
2025-07-07 13:52:27 +0900

    2025.07.07

---
+++ 250707/src/Shop/SmRepository.java
@@ -0,0 +1,12 @@
+package Shop;
+
+import java.util.List;
+
+public interface SmRepository<V, K> {
+    // Database에 CRUD 기능을 정의함
+    void insert(V v);
+    void update(V v);
+    void delete(K k);
+    List<V> selectAll();
+    V select(K k);
+}
